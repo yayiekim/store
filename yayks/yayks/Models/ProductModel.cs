cb21f6b5ea0db5ae2cd27c49c0db57d32a9ff53c
Kimberly <kabacco@nimbyx.com>
2017-02-26 20:58:10 +0800

    ghj

---
+++ yayks/yayks/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace yayks.Models
+{
+    public class ProductModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public string ImageModel { get; set; }
+    }
+}
---
+++ yayks/yayks/Models/StatusModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace yayks.Models
+{
+    public class StatusModels
+    {
+    }
+
+ 
+
+}
