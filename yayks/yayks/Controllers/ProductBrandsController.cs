a0314d98290407d7b6c225d6445429a511ed32a4
yayiekim <kimberly_abacco@live.com>
2017-04-03 11:54:47 +0800

    04/03/2017

---
+++ yayks/yayks/Controllers/ProductBrandsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using yayks;
+
+namespace yayks.Controllers
+{
+    public class ProductBrandsController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: ProductBrands
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.ProductBrands.ToListAsync());
+        }
+
+        // GET: ProductBrands/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductBrand productBrand = await db.ProductBrands.FindAsync(id);
+            if (productBrand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productBrand);
+        }
+
+        // GET: ProductBrands/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductBrands/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name")] ProductBrand productBrand)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductBrands.Add(productBrand);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(productBrand);
+        }
+
+        // GET: ProductBrands/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductBrand productBrand = await db.ProductBrands.FindAsync(id);
+            if (productBrand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productBrand);
+        }
+
+        // POST: ProductBrands/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name")] ProductBrand productBrand)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productBrand).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(productBrand);
+        }
+
+        // GET: ProductBrands/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductBrand productBrand = await db.ProductBrands.FindAsync(id);
+            if (productBrand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productBrand);
+        }
+
+        // POST: ProductBrands/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            ProductBrand productBrand = await db.ProductBrands.FindAsync(id);
+            db.ProductBrands.Remove(productBrand);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ yayks/yayks/Controllers/ProductColorsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using yayks;
+
+namespace yayks.Controllers
+{
+    public class ProductColorsController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: ProductColors
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.ProductColors.ToListAsync());
+        }
+
+        // GET: ProductColors/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductColor productColor = await db.ProductColors.FindAsync(id);
+            if (productColor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productColor);
+        }
+
+        // GET: ProductColors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductColors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,ProductColorName,ProductColorCode")] ProductColor productColor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductColors.Add(productColor);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(productColor);
+        }
+
+        // GET: ProductColors/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductColor productColor = await db.ProductColors.FindAsync(id);
+            if (productColor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productColor);
+        }
+
+        // POST: ProductColors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,ProductColorName,ProductColorCode")] ProductColor productColor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productColor).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(productColor);
+        }
+
+        // GET: ProductColors/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductColor productColor = await db.ProductColors.FindAsync(id);
+            if (productColor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productColor);
+        }
+
+        // POST: ProductColors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            ProductColor productColor = await db.ProductColors.FindAsync(id);
+            db.ProductColors.Remove(productColor);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
